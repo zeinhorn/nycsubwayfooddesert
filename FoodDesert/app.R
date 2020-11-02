b49a55852434bfd4643fdf2dd2f9f29b07106b1e
maerey32 <mereynolds@middlebury.edu>
2020-11-02 17:49:44 -0500

    added geojson
    are you able to view changes now?

--- FoodDesert/app.R
+++ FoodDesert/app.R
@@ -1,9 +1,26 @@
 library(shiny)
+<<<<<<< Updated upstream
 library(tidyverse)
 
 medianIncomes <- read_csv("MedianIncomes.csv")
+=======
+library(rvest)
+library(geojsonio)
+
+
+med.income <- read.csv(file.choose())
+nhoods <- geojson_read(file.choose(),
+                       what = "sp")
+nhoods2 <- nhoods
+nhoods3 <- left_join(nhoods2,
+                              med.income,
+                              by = "")
+#we might need to make the data as.numeric before graphing it
+>>>>>>> Stashed changes
 
 ui <- fluidPage(
+  
+  
   
 )
 
