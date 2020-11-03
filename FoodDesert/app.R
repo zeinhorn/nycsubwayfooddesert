2c4678cb9da2b963d1b17617e0629a3bf7ad1685
maerey32 <mereynolds@middlebury.edu>
2020-11-03 17:15:48 -0500

    Update app.R
    Here is what I added for the leaflet. I had tried to do a left join between the two data sets but couldn't get it to work, probably because I wasn't joining them by the correct column

--- FoodDesert/app.R
+++ FoodDesert/app.R
@@ -1,27 +1,30 @@
 library(shiny)
 library(tidyverse)
 
-medianIncomes <- read_csv("MedianIncomes.csv")
+medianIncomes <- read_csv("Median Incomes.csv")
 library(rvest)
 library(geojsonio)
+library(leaflet)
 
 
-med.income <- read.csv(file.choose())
+#med.income <- read.csv(file.choose())
 nhoods <- geojson_read(file.choose(),
                        what = "sp")
-nhoods2 <- nhoods
-nhoods3 <- left_join(nhoods2,
-                              med.income,
-                              by = "")
+nhoods.data <- nhoods
+map <- nhoods.data %>%
+  leaflet()
+
 #we might need to make the data as.numeric before graphing it
 
 ui <- fluidPage(
+  leafletOutput(outputId = "leaflet1")
   
   
   
 )
 
 server <- function(input, output, session) {
+  output$leaflet1 <- renderLeaflet
   
 }
 
