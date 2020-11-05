34a4824fa37909d99d060f1c70286a1d9a07304a
zeinhorn <34073291+zeinhorn@users.noreply.github.com>
2020-11-04 21:17:24 -0500

    create a leaflet for the cds and add income data

--- FoodDesert/app.R
+++ FoodDesert/app.R
@@ -6,35 +6,47 @@ library(rvest)
 library(geojsonio)
 library(leaflet)
 
-nhoods <- geojson_read("NYCNeighborhoods.geojson",
+nhoods <- geojson_read("CommunityDistricts.geojson",
                        what = "sp")
 nhoods.copy <- nhoods
 medianIncomesFiltered <- medianIncomes %>%
   filter(`Household Type` == "All Households" &
-         TimeFrame == 2019) %>%
-  select(Location, Data)
+           TimeFrame == 2019) %>%
+  select(Location, Data, Fips)
+
+nhoods.copy@data$boro_cd <- as.numeric(nhoods.copy@data$boro_cd)
 
 nhoods.copy@data <- left_join(nhoods.copy@data,
                               medianIncomesFiltered,
-                              by = c("ntaname" = "Location"))
+                              by = c("boro_cd" = "Fips"))
 nhoods.copy@data <- nhoods.copy@data %>%
   rename("Income" = "Data")
-  
 
-map <- nhoods.data %>%
+map <- nhoods.copy %>%
   leaflet()
 
-#we might need to make the data as.numeric before graphing it
+bins <- c(0,50000, 75000, 100000,150000, 200000, Inf)
+colors <- colorBin(palette = "YlOrRd",
+                   domain = nhoods.copy@data$Income,
+                   bins = bins)
 
 ui <- fluidPage(
-  leafletOutput(outputId = "leaflet1")
-  
-  
+  leafletOutput(outputId = "nyc_cd")
   
 )
 
 server <- function(input, output, session) {
-  output$leaflet1 <- renderLeaflet
+  output$leaflet1 <- renderLeaflet({
+    map %>% 
+      addPolygons(fillColor = ~colors(Income),
+                  color = "black",
+                  opacity = 1,
+                  fillOpacity = .7,
+                  layerId = ~Location) %>%
+      setView(-74, 40.7, 10) %>% 
+      addLegend(pal = colors,
+                values = nhoods.copy@data$Income)
+  })
   
 }
 
